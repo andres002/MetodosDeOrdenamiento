8223a18882c137d181ed362f5ab8105c7298cd1e
Andres <Andres@192.168.1.66>
2016-01-10 11:41:54 -0600

    Se agregó shell

---
+++ Ordenamientos/src/algoritmos/Shell.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmos;
+
+/**
+ *
+ * @author Andres
+ */
+public class Shell {
+   
+   public static void shell(int A[]){
+   int salto, aux, i;
+   boolean cambios;
+   for(salto=A.length/2; salto!=0; salto/=2){
+           cambios=true;
+           while(cambios){ // Mientras se intercambie algún elemento
+                       cambios=false;
+                       for(i=salto; i< A.length; i++) // se da una pasada
+                               if(A[i-salto]>A[i]){ // y si están desordenados
+                                     aux=A[i]; // se reordenan
+                                     A[i]=A[i-salto];
+                                     A[i-salto]=aux;
+                                     cambios=true; // y se marca como cambio.
+                               }
+                        }
+            }
+}
+}
